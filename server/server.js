51307b52313e7d3dd229ea2311f623d222b3c7ba
EyeVorE <subplot_05tracer@icloud.com>
2024-07-02 17:06:56 -0500

    fixed render-build

--- server/server.js
+++ server/server.js
@@ -8,9 +8,9 @@ app.use(express.static("../client/dist"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// require('./routes/htmlRoutes')(app);
-app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "../client/dist/index.html"))
-);
+require("./routes/htmlRoutes")(app);
+// app.get("/", (req, res) =>
+//   res.sendFile(path.join(__dirname, "../client/dist/index.html"))
+// );
 
 app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
