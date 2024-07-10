b48eac13772036cc8cda1d86292f5b0aeeb795fe
EyeVorE <subplot_05tracer@icloud.com>
2024-07-09 22:58:21 -0500

    still trying to figure out what is preventing deployment on render

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -19,7 +19,7 @@ export const putDb = async (content) => {
   const jateDb = await openDB("jate", 1);
   const tx = jateDb.transaction("jate", "readwrite");
   const store = tx.objectStore("jate");
-  const request = store.put({ id: 1, value: content });
+  const request = store.put({ id: 1, jate: content });
   const result = await request;
   console.log("Data saved to the database", result);
 };
@@ -44,7 +44,7 @@ export const getDb = async () => {
   // Get confirmation of the request.
   const result = await request;
   console.log("result.value", result);
-  return result;
+  return result?.jate;
 };
 
 initdb();
